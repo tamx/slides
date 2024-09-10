9efbb1b462eadcdaf079981447eeb3bbea934081
tamx <tamx@users.noreply.github.com>
2024-09-10 03:47:45 +0000

    deploy: b8f7758f4c07a6929ed30287fbba4c65e32e86cf

--- 2024/2024-09-19-js-lecture6-lesson/sample0601.js
+++ 2024/2024-09-19-js-lecture6-lesson/sample0601.js
@@ -11,5 +11,32 @@ function btn1Click() {
     display();
 }
 
+function btn2Click() {
+    price += 1500;
+    console.log(price);
+    display();
+}
+
+function btn3Click() {
+    price += 9000;
+    console.log(price);
+    display();
+}
+
+function btn4Click() {
+    price += 6000;
+    console.log(price);
+    display();
+}
+
 const elem1 = document.getElementById('btn1');
 elem1.addEventListener('click', btn1Click);
+
+const elem2 = document.getElementById('btn2');
+elem2.addEventListener('click', btn2Click);
+
+const elem3 = document.getElementById('btn3');
+elem3.addEventListener('click', btn3Click);
+
+const elem4 = document.getElementById('btn4');
+elem4.addEventListener('click', btn4Click);
